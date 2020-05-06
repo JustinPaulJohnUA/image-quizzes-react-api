92f72d5ffd3aaa144f4dc76c5fd2ed0cb51e9efe
JustinPaulJohnUA <justinpauljohn@email.arizona.edu>
2020-05-06 07:59:42 -0700

    added node_tls ignore

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 'use strict';
-//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 // dependencies 
 require('dotenv').config();
 const { Pool } = require('pg');
